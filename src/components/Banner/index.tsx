611f0f11e0c49d9f3e34658d02b7a5c7bf09f841
Renato Xavier <rnatu91@gmail.com>
2021-05-06 15:11:43 -0300

    fix: banner responsive issues

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -7,21 +7,30 @@ export function Banner(): JSX.Element {
   });
 
   return (
-    <Flex h="23rem">
+    <Flex h={['20rem', '23rem']}>
       <Box
         bgImage="url(https://images.unsplash.com/photo-1495344517868-8ebaf0a2044a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1505&q=80)"
         width="100%"
-        bgPosition="50% 15%"
-        height="20rem"
+        bgPosition={['30% 25%', '50% 25%']}
+        height={['10.18rem', '20rem']}
       >
-        <Flex mt="5rem" px="8.75rem" justify="space-between">
+        <Flex
+          mt={['1.75rem', '5rem']}
+          px={['1rem', '8.75rem']}
+          justify="space-between"
+        >
           <Box color="gray.50" w="32.75rem">
             <Text fontSize={['lg', '4xl']} fontWeight="500">
               5 Continentes, <br />
               infinitas possibilidades.
             </Text>
 
-            <Text color="gray.100" fontSize={['sm', 'xl']} fontWeight="normal">
+            <Text
+              color="gray.100"
+              fontSize={['sm', 'xl']}
+              fontWeight="normal"
+              pt="0.5rem"
+            >
               Chegou a hora de tirar do papel a viagem que você sempre sonhou.
             </Text>
           </Box>
